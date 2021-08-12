cea513058fca9460a03faefa7f4df0cfac04eca7
harshalini <harshalinipandhare29@gmail.com>
2021-08-12 18:02:06 +0530

    docs - added if...else for taken data

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ const message = document.querySelector("#result");
 checkButton.addEventListener("click", function getValues() {
     var bDate = birthDate.value;
     bDate = bDate.replaceAll("-", "");
-    console.log(bDate);
-    let sum = 0;
-    for (let i = 0; i < bDate.length; i++) {
-        sum = sum + Number(bDate.charAt(i));
-    }
-    console.log(sum);
 
-    message.style.display = "none";
-    if (sum % luckyNumber.value === 0) {
-        message.style.display = "block";
-        message.innerText = "Your  bday is lucky";
+    let sum = 0;
+
+    if (bDate && luckyNumber.value) {
+        for (let i = 0; i < bDate.length; i++) {
+            sum = sum + Number(bDate.charAt(i));
+        }
+
+        message.style.display = "none";
+        if (sum % luckyNumber.value === 0) {
+            message.style.display = "block";
+            message.innerText = "Yay! Your birthday is lucky! 🥳";
+        } else {
+            message.style.display = "block";
+            message.innerText = "Alas! Your birthday is not lucky 😕";
+        }
     } else {
-        message.style.display = "block";
-        message.innerText - "Your bday is not lucky";
+        message.innerText = "Please enter both the fields."
     }
 });
