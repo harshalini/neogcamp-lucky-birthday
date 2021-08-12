494f6b5f7369015ff462b33c65a276973290b531
harshalini <harshalinipandhare29@gmail.com>
2021-08-12 10:39:58 +0530

    feat: added display element

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const birthDate = document.querySelector("#birth-date");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#result");
 
 checkButton.addEventListener("click", function getValues() {
-    console.log(birthDate.value, luckyNumber.value);
+    var bDate = birthDate.value;
+    bDate = bDate.replaceAll("-", "");
+    console.log(bDate);
+    let sum = 0;
+    for (let i = 0; i < bDate.length; i++) {
+        sum = sum + Number(bDate.charAt(i));
+    }
+    console.log(sum);
+
+    message.style.display = "none";
+    if (sum % luckyNumber.value === 0) {
+        message.style.display = "block";
+        message.innerText = "Your  bday is lucky";
+    } else {
+        message.style.display = "block";
+        message.innerText - "Your bday is not lucky";
+    }
 });
